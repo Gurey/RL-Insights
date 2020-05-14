09c46dca9197ebef0e5c17c8b5e18ec7b155cd44
Gurey <gustav.holfve@gmail.com>
2020-05-14 21:01:24 +0200

    init commit

---
+++ src/main/main.ts
@@ -0,0 +1,78 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+declare global {
+  const MAIN_WINDOW_WEBPACK_ENTRY: string;
+}
+
+// It will change to be "true" in Electron 9.
+// For more information please check https://github.com/electron/electron/issues/18397
+app.allowRendererProcessReuse = true;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow: null | BrowserWindow;
+
+const createWindow = () => {
+  /**add chrome dev tools */
+  addReactDevTools();
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+function addReactDevTools() {
+  const devToolsModule = path.join(process.cwd(), "lib", "react-dev-tools");
+  BrowserWindow.addDevToolsExtension(devToolsModule);
+}
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
